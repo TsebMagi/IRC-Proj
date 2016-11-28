b2e4ef0f73886e32d6db047698314cb44d1e7ebc
TsebMagi <Doug.b.Whitley@gmail.com>
2016-11-28 01:25:35 -0800

    Fixing sockets

--- Server.py
+++ Server.py
@@ -25,15 +25,18 @@ class User(object):
 
 
 class Room(object):
-    def __init__(self, room_name: str, users: User=None):
+    def __init__(self, room_name, users=None):
         if users is None:
             self.users = []
         else:
-            self.users.append(users)
+            self.users = [users]
         self.room_name = room_name
 
     def add_to_room(self, user):
         self.users.append(user)
+
+    def __str__(self):
+        return self.room_name
 
 
 # list of users on the server
@@ -41,7 +44,7 @@ USERS = []
 # list of Rooms on the server
 ROOMS = []
 # used for testing, in a normal distribution of this it would be run across networks
-SERVER_ADDRESS = ("0.0.0.0", 9001)
+SERVER_ADDRESS = ("127.0.0.1", 9001)
 SERVER_SOCKET = None
 
 
@@ -58,94 +61,100 @@ signal.signal(signal.SIGINT, interrupt_handler)
 class IRCServer(socketserver.StreamRequestHandler):
 
     @staticmethod
-    def connect_process(packet: Packets.Connect, received_from: User) -> Packets.Errors:
+    def connect_process(packet, received_from):
         for user in USERS:
-            if user.handle == packet.username:
-                return Packets.Errors.USER_ALREADY_EXISTS
-        USERS.append(User(packet.username, received_from.address))
+            if user[0] == packet.username:
+                return Packets.Status.ERROR, Packets.Errors.USER_ALREADY_EXISTS
+        USERS.append((packet.username, received_from[1]))
         if testing:
-            print(USERS)
-        return Packets.Errors.NO_ERROR
+            print("Users: " + USERS.__str__())
+        return Packets.Status.OK, Packets.Errors.NO_ERROR
 
     @staticmethod
-    def disconnect_process(packet: Packets.Disconnect) -> Packets.Errors:
+    def disconnect_process(packet):
         for user in USERS:
-            if user.handle == packet.username:
-                USERS.remove(packet.username)
-                return Packets.Errors.NO_ERROR
-        return Packets.Errors.USER_NOT_FOUND
+            if user[0] == packet.username:
+                USERS.remove(user[0])
+                return Packets.Status.OK, Packets.Errors.NO_ERROR
+        return Packets.Status.ERROR, Packets.Errors.USER_NOT_FOUND
 
     @staticmethod
-    def create_room_process(packet: Packets.CreateRoom) -> Packets.Packet:
+    def create_room_process(packet):
         for room in ROOMS:
             if room.room_name == packet.room:
-                return Packets.Errors.ROOM_ALREADY_EXISTS
+                return Packets.Status.ERROR, Packets.Errors.ROOM_ALREADY_EXISTS
         ROOMS.append(Room(packet.room, packet.username))
-        return Packets.Errors.NO_ERROR
+        if testing:
+            print("Rooms: " + ROOMS.__str__())
+        return Packets.Status.OK, Packets.Errors.NO_ERROR
 
     @staticmethod
-    def join_room_process(packet: Packets.JoinRoom) ->Packets.Errors:
+    def join_room_process(packet):
         for room in ROOMS:
             if room.room_name == packet.room:
                 room.add_to_room(packet.username)
-                return Packets.Errors.NO_ERROR
+                return Packets.Status.OK, Packets.Errors.NO_ERROR
         ROOMS.append(Room(packet.room, packet.username))
         if testing:
-            print(ROOMS)
-        return Packets.Errors.NO_ERROR
+            print("Rooms: " + ROOMS.__str__())
+        return Packets.Status.OK, Packets.Errors.NO_ERROR
 
     @staticmethod
-    def leave_room_process(packet: Packets.LeaveRoom) ->Packets.Errors:
+    def leave_room_process(packet):
         for room in ROOMS:
             for user in room.users:
-                if user.handle == packet.username:
-                    room.remove(user.handle)
-                    return Packets.Errors.NO_ERROR
-        return Packets.Errors.USER_NOT_IN_ROOM
+                if user[0] == packet.username:
+                    room.remove(packet.username)
+                    return Packets.Status.OK, Packets.Errors.NO_ERROR
+        return Packets.Status.ERROR, Packets.Errors.USER_NOT_IN_ROOM
 
     @staticmethod
-    def list_rooms_process(packet: Packets.ListRooms) -> Packets.ListRooms:
+    def list_rooms_process(packet):
         packet.response = ROOMS.__str__()
         return packet
 
     @staticmethod
-    def list_members(packet: Packets.ListMembers) -> Packets.ListMembers:
+    def list_members(packet):
         for room in ROOMS:
             if room.room_name == packet.room:
                 packet.response = room.users.__str__()
                 return packet
+        packet.status = Packets.Status.ERROR
         packet.errors = Packets.Errors.ROOM_NOT_FOUND
         return packet
 
     @staticmethod
-    def message_process(self, packet: Packets.Message) -> Packets.Message:
+    def message_process(self, packet):
         for room in ROOMS:
             if room.room_name == packet.room_to_message:
                 for user in room:
                     self.send_packet(packet, user)
                 return packet
+        packet.status = Packets.Status.ERROR
         packet.errors = Packets.Errors.ROOM_NOT_FOUND
         return packet
 
     @staticmethod
-    def pm_process(self, packet: Packets.Pm) -> Packets.Message:
+    def pm_process(self, packet):
         for user in USERS:
-            if user.handle == packet.sent_to:
+            if user[0] == packet.sent_to:
                 self.send_packet(packet,user)
                 return packet
+        packet.status = Packets.Status.ERROR
         packet.errors = Packets.Errors.USER_NOT_FOUND
         return packet
 
     @staticmethod
-    def send_packet(packet: Packets.Packet, user):
+    def send_packet(packet, user):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(user.address)
+            s.connect((SERVER_ADDRESS[0], SERVER_ADDRESS[1]+100))
             s.send(packet.encode())
             s.close()
         except socket.error as e:
             if e.errno == 111:  # connection error
-                USERS.remove(user)
+                if user in USERS:
+                    USERS.remove(user)
             else:
                 print(e)
 
@@ -160,34 +169,34 @@ class IRCServer(socketserver.StreamRequestHandler):
         try:
             new_message = Packets.decode(new_input)
         except TypeError as e:
-            print("Error Processing received Packet: " + new_input + "error generated: " + e)
+            print("Error Processing received Packet: " + new_input + "error generated: " + e.__str__())
             return
         # process the input
         try:
             # check for what type of packet was sent and process appropriately
             if isinstance(new_message, Packets.Connect):
-                new_message.errors = self.connect_process(new_message,User(new_message.username, address))
+                new_message.status, new_message.errors = self.connect_process(new_message,(new_message.username, address))
             elif isinstance(new_message, Packets.Disconnect):
-                new_message.errors = self.disconnect_process(new_message)
+                new_message.status, new_message.errors = self.disconnect_process(new_message)
             elif isinstance(new_message, Packets.CreateRoom):
-                new_message.errors = self.create_room_process(new_message)
+                new_message.status, new_message.errors = self.create_room_process(new_message)
             elif isinstance(new_message,Packets.JoinRoom):
-                new_message.errors = self.join_room_process(new_message)
+                new_message.status, new_message.errors = self.join_room_process(new_message)
             elif isinstance(new_message, Packets.LeaveRoom):
-                new_message.errors = self.leave_room_process(new_message)
+                new_message.status, new_message.errors = self.leave_room_process(new_message)
             elif isinstance(new_message, Packets.Message):
-                new_message.errors = self.message_process(self,new_message)
+                new_message.status, new_message.errors = self.message_process(self,new_message)
             elif isinstance(new_message, Packets.Pm):
-                new_message.errors = self.pm_process(self,new_message)
+                new_message.status, new_message.errors = self.pm_process(self,new_message)
             elif isinstance(new_message, Packets.ListMembers):
                 new_message = self.list_members(new_message)
             elif isinstance(new_message, Packets.ListRooms):
                 new_message = self.list_rooms_process(new_message)
             else:
-                new_message.errors = Packets.Errors.MALFORMED_PACKET
+                new_message.status, new_message.errors = Packets.Status.ERROR, Packets.Errors.MALFORMED_PACKET
         # handle any type errors
         except TypeError as e:
-            new_message.errors = Packets.Errors.MALFORMED_PACKET
+            new_message.status, new_message.errors = Packets.Status.ERROR, Packets.Errors.MALFORMED_PACKET
         self.wfile.write(new_message.encode())
         return
 
